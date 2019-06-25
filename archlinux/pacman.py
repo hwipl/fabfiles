c38aa6436b37dd20868aa539a4f424b2acc5443c
hwipl <33433250+hwipl@users.noreply.github.com>
2019-06-25 16:48:31 +0200

    archlinux: add find, (un)install, update packages tasks
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- archlinux/pacman.py
+++ archlinux/pacman.py
@@ -8,7 +8,46 @@ from fabric import task
 @task
 def get_installed_packages(conn):
     """
-    Get list of installed packages with pacman
+    Get list of installed packages
     """
 
     conn.run("pacman -Qqe")
+
+
+@task
+def find_packages(conn, search=None):
+    """
+    Find packages matching given search string
+    """
+
+    if search:
+        conn.run("pacman -Ss {}".format(search))
+
+
+@task
+def install_packages(conn, packages=None):
+    """
+    Install given packages
+    """
+
+    if packages:
+        conn.run("pacman -S {}".format(packages))
+
+
+@task
+def uninstall_packages(conn, packages=None):
+    """
+    Uninstall given packages
+    """
+
+    if packages:
+        conn.run("pacman -R {}".format(packages))
+
+
+@task
+def update_packages(conn):
+    """
+    Update all installed packages
+    """
+
+    conn.run("pacman -Syu")
